404dd70615449c540b864fb82e3c704f6d826efd
JONS STEEL <data@jonssteel.com>
2024-05-07 16:34:11 +0600

    minify and non-minify ok

--- webpack/webpack-4/webpack.config.js
+++ webpack/webpack-4/webpack.config.js
@@ -1,14 +1,33 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+// Check if webpack is running in production mode
+const isProduction = process.env.NODE_ENV === 'production';
 
 const entry = {
   app: './src/js/app.js',
   // index: './src/js/index.js',
 }
 
+const optimization = {
+  minimize: isProduction, // if true/false
+  minimizer: [
+    // For JavaScript
+    new TerserPlugin({
+      test: /\.js(\?.*)?$/i,
+      extractComments: false,
+    }),
+    // For CSS
+    new CssMinimizerPlugin(),
+  ],
+};
 
 const config = {
+  
+  mode: isProduction ? 'production' : 'development', // Set mode based on environment
 
   entry: entry,
 
@@ -56,11 +75,15 @@ const config = {
       }
     ]
   },
+  
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name].css'
     })
-  ]
+  ],
+
+  optimization: optimization,
+
 };
 
 module.exports = config;
